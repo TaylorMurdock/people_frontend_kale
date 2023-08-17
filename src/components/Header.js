96da3b8755a3925082b34749bb07d47c7ba7c615
Taylor Murdock <tjmurd4545@gmail.com>
2023-08-16 21:48:01 -0400

    frontend auth done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-function Header(props){
-    return <nav className="nav">
-        <Link to="/">
-            <div>People App</div>
-        </Link>
+function Header(props) {
+  return (
+    <nav className="nav">
+      <Link to="/dashboard">
+        <div>People App</div>
+      </Link>
     </nav>
-
+  );
 }
 
-export default Header
+export default Header;
